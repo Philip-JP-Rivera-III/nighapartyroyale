bb5e46ee7546b20bcebffe607c13fd13fbc81747
Phil <pjpriii@gmail.com>
2013-06-07 15:21:32 -0400

    First commitgit commit -mgit commit -mXD

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nighapartyroyale::Application.config.secret_token = 'a88e3497d026ea65844d7d9fd18d1364f74b1f1447f31b670dc259ae43a7cd66cc91f95e4b9b2dadbc0b422af38fa368f09af0abe6788365fa1cb83ce35a8327'
